b641526ae5e2bd0e0bd0f331ee9ceaa8b09a9de3
enriqueamoretti <enrique.eoal@gmail.com>
2025-09-27 09:26:21 -0500

    Implement RecyclerView with Coil to display a list of countries
    - Add Coil dependency for image loading.
    - Create CountryModel data class.
    - Update HomeScreen to use LazyColumn for displaying a list of countries from mock data.
    - Update AGP and Navigation Compose versions.

---
+++ app/src/main/java/dev/eamoretti/appue/data/model/CountryModel.kt
@@ -0,0 +1,8 @@
+package dev.eamoretti.appue.data.model
+
+import android.service.notification.NotificationListenerService
+
+data class CountryModel(
+    val name: String, val ranking: Int, val imageURL: String
+
+)
--- app/src/main/java/dev/eamoretti/appue/presentation/home/HomeScreen.kt
+++ app/src/main/java/dev/eamoretti/appue/presentation/home/HomeScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,24 +16,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import dev.eamoretti.appue.data.model.CountryModel
+
+val mockCountries = listOf(
+    CountryModel("Colombia", 8, "https://flagcdn.com/w320/co.png"),
+    CountryModel("Argentina", 1, "https://flagcdn.com/w320/ar.png"),
+    CountryModel("Francia", 4, "https://flagcdn.com/w320/fr.png"),
+    CountryModel("Brasil", 3, "https://flagcdn.com/w320/br.png"),
+    CountryModel("Perú", 52, "https://flagcdn.com/w320/pe.png"),
+    CountryModel("Uruguay", 9, "https://flagcdn.com/w320/ur.png")
+)
 
 @Composable
 fun HomeScreen(navController: NavController) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        //verticalArrangement = Arrangement.Center,
+        //horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Home Screen", style = MaterialTheme.typography.titleLarge)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("login") }) {
-            Text("Go to Login")
-        }
+        //Bienvenido a la pantalla principal
+        Text(text = "Ranking FIFA 2025")
         Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = { navController.navigate("register") }) {
-            Text("Go to Register")
+
+        LazyColumn {
+            items(mockCountries) { country ->
+                Text("Country: ${country.name}")
+            }
         }
     }
 }
